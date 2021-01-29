d6e5a39c88f79f84ef428201c335f5bcc6c65122
Alexey Reznichenko <raleksey2005@gmail.com>
2021-01-29 20:50:30 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="body">
+    <main-page></main-page>
+  </div>
+</template>
+
+<script>
+  import MainPage from './components/MainPage'
+
+  export default {
+    components: {
+      'main-page': MainPage
+    }
+  }
+</script>
+
+<style lang="scss">
+  
+</style>
